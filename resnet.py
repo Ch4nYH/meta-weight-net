114fa894430b33a5962648eafef53b0061209c23
Ch4nYH <hfercxx@hotmail.com>
2020-05-11 17:48:35 +0800

    update

--- resnet.py
+++ resnet.py
@@ -196,6 +196,95 @@ def _weights_init(m):
     if isinstance(m, MetaLinear) or isinstance(m, MetaConv2d):
         init.kaiming_normal(m.weight)
 
+class MetaSequential(nn.Sequential):
+
+    def __init__(self, *args):
+        super(MetaSequential, self).__init__(*args)
+
+    def parameters(self):
+       for name, param in self.named_params(self):
+            yield param
+    
+    def named_leaves(self):
+        return []
+    
+    def named_submodules(self):
+        return []
+    
+    def named_params(self, curr_module=None, memo=None, prefix=''):       
+        if memo is None:
+            memo = set()
+
+        if hasattr(curr_module, 'named_leaves'):
+            for name, p in curr_module.named_leaves():
+                if p is not None and p not in memo:
+                    memo.add(p)
+                    yield prefix + ('.' if prefix else '') + name, p
+        else:
+            for name, p in curr_module._parameters.items():
+                if p is not None and p not in memo:
+                    memo.add(p)
+                    yield prefix + ('.' if prefix else '') + name, p
+                    
+        for mname, module in curr_module.named_children():
+            submodule_prefix = prefix + ('.' if prefix else '') + mname
+            for name, p in self.named_params(module, memo, submodule_prefix):
+                yield name, p
+
+    def update_params(self, lr_inner, first_order=False, source_params=None, detach=False):
+        if source_params is not None:
+            for tgt, src in zip(self.named_params(self), source_params):
+                name_t, param_t = tgt
+                # name_s, param_s = src
+                # grad = param_s.grad
+                # name_s, param_s = src
+                grad = src
+                if first_order:
+                    grad = to_var(grad.detach().data)
+                if grad is None:
+                    print(name_t)
+                    tmp = param_t
+                else:
+                    tmp = param_t - lr_inner * grad
+                self.set_param(self, name_t, tmp)
+        else:
+            for name, param in self.named_params(self):
+                if not detach:
+                    grad = param.grad
+                    if first_order:
+                        grad = to_var(grad.detach().data)
+                    if grad is None:
+                        print(name)
+                        tmp = param_t
+                    else:
+                        tmp = param_t - lr_inner * grad
+                    self.set_param(self, name, tmp)
+                else:
+                    param = param.detach_()
+                    self.set_param(self, name, param)
+
+    def set_param(self,curr_mod, name, param):
+        if '.' in name:
+            n = name.split('.')
+            module_name = n[0]
+            rest = '.'.join(n[1:])
+            for name, mod in curr_mod.named_children():
+                if module_name == name:
+                    self.set_param(mod, rest, param)
+                    break
+        else:
+            setattr(curr_mod, name, param)
+            
+    def detach_params(self):
+        for name, param in self.named_params(self):
+            self.set_param(self, name, param.detach())   
+                
+    def copy(self, other, same_var=False):
+        for name, param in other.named_params():
+            if not same_var:
+                param = to_var(param.data.clone(), requires_grad=True)
+            self.set_param(name, param)
+
 class LambdaLayer(MetaModule):
     def __init__(self, lambd):
         super(LambdaLayer, self).__init__()
@@ -224,7 +313,7 @@ class BasicBlock(MetaModule):
                 self.shortcut = LambdaLayer(lambda x:
                                             F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
             elif option == 'B':
-                self.shortcut = nn.Sequential(
+                self.shortcut = MetaSequential(
                     MetaConv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
                     MetaBatchNorm2d(self.expansion * planes)
                 )
@@ -249,7 +338,7 @@ class ResNet32(MetaModule):
 
         self.apply(_weights_init)
         
-        self.backbone = nn.Sequential(
+        self.backbone = MetaSequential(
             self.conv1,
             self.bn1,
             nn.ReLU(inplace = True),
